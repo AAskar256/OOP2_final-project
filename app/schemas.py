19c6e85c91d8016c19256b9c0d7efdf0e9351743
codesilentkiller <167834653+codesilentkiller@users.noreply.github.com>
2025-06-14 17:46:31 +0000

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -7,7 +7,8 @@ class Token(BaseModel):
     token_type: str
 
 class TokenData(BaseModel):
-    email: Optional[str] = None
+    email: Optional[EmailStr] = None
+
 
 class UserBase(BaseModel):
     email: EmailStr
@@ -16,43 +17,16 @@ class UserBase(BaseModel):
 class UserCreate(UserBase):
     password: str
 
-class User(UserBase):
+class User(BaseModel):
     id: int
+    email: EmailStr
+    full_name: str
     is_active: bool
     role: str
 
     class Config:
         orm_mode = True
 
-class PlayBase(BaseModel):
-    title: str
-    description: str
-    genre: str
-    duration_minutes: int
-
-class PlayCreate(PlayBase):
-    director_id: int
-
-class Play(PlayBase):
-    id: int
-    director_id: int
-
-    class Config:
-        orm_mode = True
-
-class ActorBase(BaseModel):
-    full_name: str
-    bio: str
-    nationality: str
-
-class ActorCreate(ActorBase):
-    pass
-
-class Actor(ActorBase):
-    id: int
-
-    class Config:
-        orm_mode = True
 
 class DirectorBase(BaseModel):
     full_name: str
@@ -68,47 +42,85 @@ class Director(DirectorBase):
     class Config:
         orm_mode = True
 
+
+class ActorBase(BaseModel):
+    full_name: str
+    bio: str
+    nationality: str
+
+class ActorCreate(ActorBase):
+    pass
+
+class Actor(ActorBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class PlayBase(BaseModel):
+    title: str
+    description: str
+    genre: str
+    duration_minutes: int
+
+class PlayCreate(PlayBase):
+    director_id: int
+
+class Play(PlayBase):
+    id: int
+    director: Optional[Director]  # Nested director
+
+    class Config:
+        orm_mode = True
+
+
 class ShowtimeBase(BaseModel):
-    play_id: int
     start_time: datetime
     end_time: datetime
     venue: str
     available_seats: int
 
 class ShowtimeCreate(ShowtimeBase):
-    pass
+    play_id: int
 
 class Showtime(ShowtimeBase):
     id: int
+    play: Play  # Nested play
 
     class Config:
         orm_mode = True
 
+
 class TicketBase(BaseModel):
-    showtime_id: int
-    customer_id: int
     seat_number: str
     price: float
 
 class TicketCreate(TicketBase):
-    pass
+    showtime_id: int
+    customer_id: Optional[int]  # Might come from authenticated user
 
 class Ticket(TicketBase):
     id: int
+    showtime: Showtime
+    customer: User
     booking_time: datetime
     is_paid: bool
 
     class Config:
         orm_mode = True
 
+
 class ActorPlayAssociationBase(BaseModel):
-    actor_id: int
-    play_id: int
     role_name: str
 
 class ActorPlayAssociationCreate(ActorPlayAssociationBase):
-    pass
+    actor_id: int
+    play_id: int
 
 class ActorPlayAssociation(ActorPlayAssociationBase):
+    actor: Actor
+    play: Play
+
     class Config:
         orm_mode = True
